819aa843371d1bca741c85433394d3693a7ebb69
ansh20202020 <anshuman.gem@gmail.com>
2025-08-13 01:24:19 +0530

    Update server.js CORS

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use('/api/', limiter);
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', "https://inventory-new-henna.vercel.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
