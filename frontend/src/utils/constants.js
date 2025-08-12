10e1a0c0d7380cc357cb6a48261289ecc1a3fb01
ansh20202020 <anshuman.gem@gmail.com>
2025-08-13 00:44:55 +0530

    Update constants.js with render

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'https://inventory-new-hk39.onrender.com/';
 
 export const PRODUCT_CATEGORIES = [
   'Electronics',
